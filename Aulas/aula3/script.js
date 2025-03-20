9f99f3071b24db303fb6cf8c26165d0e388e1aeb
JoaoPedrosz <joaosilvajs013@gmail.com>
2025-03-20 15:54:55 -0300

    dsa

--- Aulas/aula3/script.js
+++ Aulas/aula3/script.js
@@ -4,12 +4,14 @@ function validarFormulario() {
   let ra = document.getElementById("ra").value;
   let nome = document.getElementById("nome").value;
   let email = document.getElementById("email").value;
-  let senha = document.getElementById("senha").value;
+  let senha = document.getElementById("password").value;
   let ra_ano = ra.slice(0, 4);
 
-  if (ra === "" || nome === "" || email === "" || senha === "") {
+  if(ra === "" || nome === "" || email === "" || senha === "") {
     console.log("Preencha todos os campos");
     return false;
   }
-  return true;
+
+  if(ra.lengh() != 9)
+  
 }
